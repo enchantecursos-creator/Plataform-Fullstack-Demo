6dc4d8ca6d376feb8f36ca6c2e791401dc385fb8
Gabriel do Nascimento Alves <enchantecursos@gmail.com>
2026-01-30 02:25:26 -0300

    Update Supabase client URL and anon key

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lpcgxcpjildyoalxvato.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwY2d4Y3BqaWxkeW9hbHh2YXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzMjkxNDIsImV4cCI6MjA3MzkwNTE0Mn0.nrNrAPKqHLAR5aoQlcvdHX53n1EhEti06j2Z7yracnI';
+const supabaseUrl = 'https://mirznibtnlxnqionjmlz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pcnpuaWJ0bmx4bnFpb25qbWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDM4ODMsImV4cCI6MjA4NTMxOTg4M30.LJTmDV0kw60KeIOrY6rWZjUAaerXHjQQVSSwBE9bpus';
 
 const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
